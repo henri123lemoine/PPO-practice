1a41762391e259dc12a798a4b4fa94d622c96c07
Henri Lemoine <henri123lemoine@gmail.com>
2024-09-11 19:21:10 -0400

    DummyVecEnv'd all environments

--- src/environments/env_factory.py
+++ src/environments/env_factory.py
@@ -9,12 +9,11 @@ from src.environments.mujoco_humanoid import MuJoCoHumanoidEnv
 def create_env(config: Config, env_name: str):
     if env_name == "simple_humanoid":
         env = SimpleHumanoidEnv()
-        env = Monitor(env, str(MONITOR_FILE))
-        env = DummyVecEnv([lambda: env])
     elif env_name == "mujoco_humanoid":
         env = MuJoCoHumanoidEnv()
-        env = Monitor(env, str(MONITOR_FILE))
     else:
         raise ValueError(f"Unknown environment: {env_name}")
 
+    env = Monitor(env, str(MONITOR_FILE))
+    env = DummyVecEnv([lambda: env])
     return env
