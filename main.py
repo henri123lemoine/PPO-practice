b151eab461e889d6ef2dcdc34eb63f1d44a7b32f
Henri Lemoine <henri123lemoine@gmail.com>
2024-09-11 18:44:11 -0400

    Added logging

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+import logging
+
 from src.experiments.experiment_1 import main as experiment_1
 from src.experiments.mujoco_experiment import main as mujoco_experiment
+
+logging.basicConfig(
+    level=logging.NOTSET,
+    format=f"[%(levelname)s|%(module)s|L%(lineno)d] %(asctime)s: %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S%z",
+)
+logging.getLogger("matplotlib.font_manager").setLevel(
+    logging.ERROR
+)  # Deactivate bugged matplotlib DEBUG logger
+logger = logging.getLogger(__name__)
+
 
 def main():
     # Choose which experiment to run
     # experiment_1()
     mujoco_experiment()
+
 
 if __name__ == "__main__":
     main()
