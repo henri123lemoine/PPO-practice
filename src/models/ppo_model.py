57f2e868adec2fbc1f906864f08c4151388c603a
Henri Lemoine <henri123lemoine@gmail.com>
2024-09-11 02:37:57 -0400

    Added ppo params to PPO model creation

--- src/models/ppo_model.py
+++ src/models/ppo_model.py
@@ -1,5 +1,5 @@
 from stable_baselines3 import PPO
 
 
-def create_ppo_model(env, tensorboard_log: str) -> PPO:
-    return PPO("MlpPolicy", env, verbose=1, tensorboard_log=tensorboard_log)
+def create_ppo_model(env, tensorboard_log: str, ppo_params: dict[str, int | float]) -> PPO:
+    return PPO("MlpPolicy", env, verbose=1, tensorboard_log=tensorboard_log, **ppo_params)
