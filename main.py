910bf8f14f0471367695a38fa12553029b97db2f
Henri Lemoine <henri123lemoine@gmail.com>
2024-09-11 18:26:47 -0400

    Made main's default experiment the mujoco experiment

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from experiments.experiment_1 import main as experiment_1
-
+from src.experiments.experiment_1 import main as experiment_1
+from src.experiments.mujoco_experiment import main as mujoco_experiment
 
 def main():
-    experiment_1()
-
+    # Choose which experiment to run
+    # experiment_1()
+    mujoco_experiment()
 
 if __name__ == "__main__":
     main()
