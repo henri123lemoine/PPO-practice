22980521e90f2c9400092682c129de662b303808
Henri Lemoine <henri123lemoine@gmail.com>
2024-09-11 23:32:37 -0400

    Simplified factory and applied VecVideoRecorder on VecEnv

--- src/environments/env_factory.py
+++ src/environments/env_factory.py
@@ -1,40 +1,20 @@
 import gymnasium as gym
-from gymnasium.wrappers import RecordVideo
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import VecVideoRecorder
 
 from src.config import Config
+from src.environments import simple_humanoid, mujoco_humanoid
 
 
 def make_env(config: Config):
-
     if config.env_name == "simple_humanoid":
-        from src.environments.simple_humanoid import SimpleHumanoidEnv
-        env = SimpleHumanoidEnv()
-    elif config.env_name == "mujoco_humanoid":
-        from src.environments.mujoco_humanoid import MuJoCoHumanoidEnv
-        # env = MuJoCoHumanoidEnv()  # this didn't work because it doesn't behave sufficiently like any other gym environments
+        env = simple_humanoid.SimpleHumanoidEnv()
+    elif config.env_name == "Humanoid-v4":
         env = gym.make("Humanoid-v4", render_mode="rgb_array")
     else:
         raise ValueError(f"Unknown environment: {config.env_name}")
-
-    if config.monitor:
-        env = Monitor(
-            env,
-            filename=str(config.experiment_path),
-        )
-
-    if config.record_video:
-        env = RecordVideo(
-            env,
-            video_folder=str(config.videos_path),
-            episode_trigger=config.episode_trigger,
-            step_trigger=None,
-            video_length=0,
-            name_prefix="rl-video",
-            disable_logger=False,
-        )
-
+    env = Monitor(env)
     return env
 
 def create_env(config: Config):
@@ -43,4 +23,12 @@ def create_env(config: Config):
         n_envs=config.n_envs,
         seed=config.seed,
     )
+    if config.record_video:
+        env = VecVideoRecorder(
+            env,
+            video_folder=str(config.videos_path),
+            record_video_trigger=config.record_video_trigger,
+            video_length=config.record_video_length,
+            name_prefix=None,
+        )
     return env
