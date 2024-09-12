7ef8dc46a4664871b75c60833dafdb6d422ca53a
Henri Lemoine <henri123lemoine@gmail.com>
2024-09-11 22:26:02 -0400

    Fixed multi-env setup

--- src/environments/env_factory.py
+++ src/environments/env_factory.py
@@ -4,39 +4,43 @@ from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.monitor import Monitor
 
 from src.config import Config
-from src.environments.simple_humanoid import SimpleHumanoidEnv
-from src.environments.mujoco_humanoid import MuJoCoHumanoidEnv
 
 
-def create_env(config: Config):
+def make_env(config: Config):
+
     if config.env_name == "simple_humanoid":
+        from src.environments.simple_humanoid import SimpleHumanoidEnv
         env = SimpleHumanoidEnv()
     elif config.env_name == "mujoco_humanoid":
+        from src.environments.mujoco_humanoid import MuJoCoHumanoidEnv
         # env = MuJoCoHumanoidEnv()  # this didn't work because it doesn't behave sufficiently like any other gym environments
         env = gym.make("Humanoid-v4", render_mode="rgb_array")
     else:
         raise ValueError(f"Unknown environment: {config.env_name}")
-    
+
     if config.monitor:
         env = Monitor(
             env,
             filename=str(config.experiment_path),
         )
-    
+
     if config.record_video:
         env = RecordVideo(
             env,
             video_folder=str(config.videos_path),
-            episode_trigger=lambda step_id: step_id % 100 == 0,
+            episode_trigger=config.episode_trigger,
             step_trigger=None,
             video_length=0,
             name_prefix="rl-video",
             disable_logger=False,
         )
 
+    return env
+
+def create_env(config: Config):
     env = make_vec_env(
-        lambda: env,
-        n_envs = 1,
-        seed = config.seed,
+        env_id=lambda: make_env(config),
+        n_envs=config.n_envs,
+        seed=config.seed,
     )
     return env
