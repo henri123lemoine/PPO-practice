835162d4e0d89c7ec455c89143c381a63e970d3f
Henri Lemoine <henri123lemoine@gmail.com>
2024-09-11 21:13:23 -0400

    Added configuration for env-factory, e.g. video

--- src/environments/env_factory.py
+++ src/environments/env_factory.py
@@ -1,7 +1,8 @@
-from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines3.common.monitor import Monitor
+import gymnasium as gym
+from gymnasium.wrappers import RecordVideo
+from stable_baselines3.common.env_util import make_vec_env
 
-from src.config import Config, MONITOR_FILE
+from src.config import Config
 from src.environments.simple_humanoid import SimpleHumanoidEnv
 from src.environments.mujoco_humanoid import MuJoCoHumanoidEnv
 
@@ -10,10 +11,31 @@ def create_env(config: Config, env_name: str):
     if env_name == "simple_humanoid":
         env = SimpleHumanoidEnv()
     elif env_name == "mujoco_humanoid":
-        env = MuJoCoHumanoidEnv()
+        # env = MuJoCoHumanoidEnv()
+        env = gym.make("Humanoid-v4", render_mode="rgb_array")
     else:
         raise ValueError(f"Unknown environment: {env_name}")
 
-    env = Monitor(env, str(MONITOR_FILE))
-    env = DummyVecEnv([lambda: env])
+    env = make_vec_env(
+        env_id = lambda: env, env_kwargs = None,
+
+        n_envs = 1,
+        seed = config.seed,
+        start_index = 0,
+
+        monitor_dir = str(config.experiment_path), monitor_kwargs=None,
+
+        wrapper_class = RecordVideo,
+        wrapper_kwargs={
+            "video_folder": str(config.videos_path),
+            "episode_trigger": lambda step_id: step_id % 100 == 0,
+            "step_trigger": None,
+            "video_length": 0,
+            "name_prefix": "rl-video",
+            "disable_logger": False
+        },
+
+        # A custom ``VecEnv`` class constructor, and kwargs
+        vec_env_cls = None, vec_env_kwargs = None,
+    )
     return env
