4a4546e23e266311f9add1f27d331ca099fc25ae
Henri Lemoine <henri123lemoine@gmail.com>
2024-09-11 21:16:54 -0400

    Added comment to MuJoCoHumanoidEnv

--- src/environments/mujoco_humanoid.py
+++ src/environments/mujoco_humanoid.py
@@ -6,6 +6,26 @@ from src.environments.base_env import BaseEnv
 
 
 class MuJoCoHumanoidEnv(BaseEnv):
+    """
+    Try out all of these:
+
+    Humanoid-v2
+    Humanoid-v3
+    Humanoid-v4
+    HumanoidStandup-v2
+    HumanoidStandup-v4
+
+    Walker2d-v2
+    Walker2d-v3
+    Walker2d-v4
+
+    BipedalWalker-v3
+    BipedalWalkerHardcore-v3
+
+    See https://gymnasium.farama.org/environments/ for more information.
+    See https://github.com/duburcqa/jiminy for jiminy environments.
+    """
+
     def __init__(self):
         super().__init__()
         self.env = gym.make('Humanoid-v4')
